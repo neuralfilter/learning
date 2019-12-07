8c54c1165700aad32387ef8747d6f40b4c855e79
neuralfilter <seiji.sakurai@outlook.com>
2019-12-07 17:14:00 -0500

    more files!

---
+++ head_first_c/card_check.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int card_count = 10;
+	if(card_count > 5){
+		puts("Too many cards!");
+	}
+	return 0;
+}
---
+++ head_first_c/rocks.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	puts("My first C program!");
+	printf("This is printf B)\n");
+	return 0;
+}
